b6f03b1a7c5fd8d913a858b90ce1be3b68de3b45
Puskar-Roy <puskarroy600@gmail.com>
2024-04-08 10:12:51 +0530

    feat : login form data

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import RegisterImage from "../assets/RegisterImage";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
   return (
     <div className="min-w-screen min-h-screen  flex items-center justify-center px-5 py-5">
       <div
@@ -17,38 +27,47 @@ const Login = () => {
               <h1 className="font-bold text-3xl text-gray-900">LOGIN</h1>
               <p>Enter your information to login</p>
             </div>
-            <div>
-              <label htmlFor="email" className="block mb-2">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full border border-gray-300 rounded-md px-3 py-2 mb-5"
-              />
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="email" className="block mb-2">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full border border-gray-300 rounded-md px-3 py-2 mb-5"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
 
-              <label htmlFor="password" className="block mb-2">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                className="w-full border border-gray-300 rounded-md px-3 py-2 mb-5"
-              />
+                <label htmlFor="password" className="block mb-2">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="w-full border border-gray-300 rounded-md px-3 py-2 mb-5"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
 
-              <div className="flex gap-1 justify-end mb-3 sm:mb-0">
-                Do not have an accout?
-                <Link to="/register" className="text-indigo-500">
-                  Register
-                </Link>
+                <div className="flex gap-1 justify-end mb-3 sm:mb-0">
+                  Do not have an account?
+                  <Link to="/register" className="text-indigo-500">
+                    Register
+                  </Link>
+                </div>
+
+                <button
+                  type="submit"
+                  className="bg-indigo-500 text-white font-semibold px-4 py-2 rounded-md hover:bg-indigo-600"
+                >
+                  Login
+                </button>
               </div>
-
-              <button className="bg-indigo-500 text-white font-semibold px-4 py-2 rounded-md hover:bg-indigo-600">
-                Login
-              </button>
-            </div>
+            </form>
           </div>
         </div>
       </div>
