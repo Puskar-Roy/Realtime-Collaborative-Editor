d3adab115facf93cbb6cddfa484da781d57713b7
Puskar-Roy <puskarroy600@gmail.com>
2024-04-10 15:11:48 +0530

    fix : socket connection

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -7,5 +7,5 @@ export const initSocket = async () => {
     timeout: 10000,
     transports: ["websocket"],
   };
-  return io("https://realtimecollaborativeeditorbackend.vercel.app", options);
+  return io(`${import.meta.env.VITE_API}`, options);
 };
