a70fd8938f4f66a92e57e426c5d128661706398f
Atul <oraonatula@gmail.com>
2024-04-26 23:27:57 +0530

    Added the GoogleAuth below the form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -39,65 +39,67 @@ const Login = () => {
             {isLoading ? (
               <Loder />
             ) : (
-              <form onSubmit={handleSubmit} ref={formRef}>
-                <div>
-                  <label htmlFor="email" className="block mb-2">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    className="w-full border border-gray-300 rounded-md px-3 py-2 mb-5"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
+              <>
+                <form onSubmit={handleSubmit} ref={formRef}>
+                  <div>
+                    <label htmlFor="email" className="block mb-2">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      className="w-full border border-gray-300 rounded-md px-3 py-2 mb-5"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
 
-                  <label htmlFor="password" className="block mb-2">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    className="w-full border border-gray-300 rounded-md px-3 py-2 mb-5"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
+                    <label htmlFor="password" className="block mb-2">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      className="w-full border border-gray-300 rounded-md px-3 py-2 mb-5"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
 
-                  <div className="flex gap-1 justify-end mb-3 sm:mb-0">
-                    Do not have an account?
-                    <Link to="/register" className="text-indigo-500">
-                      Register
-                    </Link>
-                  </div>
-
-                  <button
-                    disabled={isLoading}
-                    type="submit"
-                    className="bg-indigo-500 text-white font-semibold px-4 py-2 rounded-md hover:bg-indigo-600"
-                  >
-                    Login
-                  </button>
-                  <div className="oauth mt-4">
-                    <p className="text-center">Or Login with</p>
-                    <div className="flex justify-center mt-4">
-                      <GoogleAuth />
+                    <div className="flex gap-1 justify-end mb-3 sm:mb-0">
+                      Do not have an account?
+                      <Link to="/register" className="text-indigo-500">
+                        Register
+                      </Link>
                     </div>
+
+                    <button
+                      disabled={isLoading}
+                      type="submit"
+                      className="bg-indigo-500 text-white font-semibold px-4 py-2 rounded-md hover:bg-indigo-600"
+                    >
+                      Login
+                    </button>
+                  </div>
+                  {error && (
+                    <div className="bg-rose-200 text-rose-500 p-5 rounded-lg mt-4">
+                      Invalid credentials or Email is not verified, if email not
+                      verified We will send a email into your account.
+                    </div>
+                  )}
+                  {isSucess && (
+                    <div className="bg-green-200 text-green-500 p-5 rounded-lg mt-4">
+                      Login Done!
+                    </div>
+                  )}
+                </form>
+                <div className="oauth mt-4">
+                  <p className="text-center">Or Login with</p>
+                  <div className="flex justify-center mt-4">
+                    <GoogleAuth />
                   </div>
                 </div>
-                {error && (
-                  <div className="bg-rose-200 text-rose-500 p-5 rounded-lg mt-4">
-                    Invalid credentials or Email is not verified, if email not
-                    verified We will send a email into your account.
-                  </div>
-                )}
-                {isSucess && (
-                  <div className="bg-green-200 text-green-500 p-5 rounded-lg mt-4">
-                    Login Done!
-                  </div>
-                )}
-              </form>
+              </>
             )}
           </div>
         </div>
